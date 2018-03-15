03d1be4a2b3c0580d091e69c08a00164968bb879
Cihan Bebek <cihan.m.bebek@gmail.com>
2018-03-15 11:19:20 +0200

    add styles for post-page

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,12 +1,89 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+
+const PostImage = styled.div`
+  margin: 2rem 0 1rem 0;
+  width: 100%;
+  height: 20rem;
+  position: relative;
+  z-index: 10;
+  background-size: cover;
+  background-position: 50% 50%;
+`;
+
+const PostHeader = styled.h1`
+  position: absolute;
+  z-index: 20;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 1rem;
+  color: white;
+
+  &:after {
+    content: "";
+    position: absolute;
+    z-index: -1;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 5rem;
+    background-image: linear-gradient(
+      to top,
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0)
+    );
+  }
+`;
+
+const PostMeta = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 2rem;
+`;
+
+const PostDate = styled.small`
+  font-size: 12px;
+`;
+
+const PostTags = styled.span`
+  display: inline-block;
+  padding: 0.2rem 0.5rem;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  font-size: 10px;
+  font-weight: bold;
+  background: #e74c3c;
+  color: white;
+  border-radius: 20px;
+`;
+
+const MainContent = styled.div`
+
+  p {
+    margin-top: 1rem;
+  }
+`;
 
 export default ({ data }) => {
   const post = data.markdownRemark;
+
   return (
     <div>
-      <img src={post.frontmatter.thumbnailUrl} alt={post.frontmatter.title} />
-      <h1>{post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <PostImage
+        style={{
+          backgroundImage: `url(${post.frontmatter.thumbnailUrl})`
+        }}
+      >
+        <PostHeader>{post.frontmatter.title}</PostHeader>
+      </PostImage>
+      <PostMeta>
+          <PostDate>{post.frontmatter.date}</PostDate>
+          <PostTags>{post.frontmatter.tags}</PostTags>
+      </PostMeta>
+      <MainContent dangerouslySetInnerHTML={{ __html: post.html }} />
     </div>
   );
 };
@@ -18,6 +95,8 @@ export const query = graphql`
       frontmatter {
         title
         thumbnailUrl
+        tags
+        date(formatString: "DD.M.YYYY")
       }
     }
   }
